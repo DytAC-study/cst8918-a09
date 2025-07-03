5d3d778ea9f50519a69b49a999339c97b348c1b2
DytAC-study <du000086@algonquinlive.com>
2025-07-03 17:46:55 -0400

    Test: bad formatting

---
+++ infrastructure/storage.tf
@@ -0,0 +1,16 @@
+# infrastructure/main.tf
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "Canada Central"
+}
+
+resource "azurerm_storage_account" "example" {
+        name                     = "examplestorage2025" resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
